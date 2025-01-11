585b60cb6d0991f998e19cbba4547823b64e30db
Fathony Teguh Irawan <4829378+fathonyfath@users.noreply.github.com>
2025-01-11 23:39:01 +0900

    Additional vertex buffer attribute

--- src/render.zig
+++ src/render.zig
@@ -1,47 +1,43 @@
 const std = @import("std");
 const gl = @import("gl");
 const Shader = @import("Shader.zig");
+const rgfw = @import("rgfw.zig").rgfw;
 
 const vertex_shader_source =
     \\#version 330 core
     \\layout(location = 0) in vec3 aPos;
+    \\layout(location = 1) in vec3 aColor;
+    \\
+    \\out vec3 ourColor;
     \\
     \\void main() {
     \\  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    \\  ourColor = aColor;
     \\}
     \\
 ;
 
 const fragment_shader_source =
     \\#version 330 core
+    \\in vec3 ourColor;
+    \\
     \\out vec4 FragColor;
     \\
     \\void main() {
-    \\  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    \\  FragColor = vec4(ourColor, 1.0);
     \\}
     \\
 ;
 
 const vertices_triangle = [_]f32{
-    -0.5, -0.5, 0.0,
-    0.5,  -0.5, 0.0,
-    0.0,  0.5,  0.0,
+    // positions     // colors
+    0.5,  -0.5, 0.0, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+    0.0,  0.5,  0.0, 0.0, 0.0, 1.0,
 };
 
 const indices_triangle = [_]u32{
     0, 1, 2,
-};
-
-const vertices_square = [_]f32{
-    0.5,  0.5,  0.0,
-    0.5,  -0.5, 0.0,
-    -0.5, -0.5, 0.0,
-    -0.5, 0.5,  0.0,
-};
-
-const indices_square = [_]u32{
-    0, 1, 3,
-    1, 2, 3,
 };
 
 var VAO: gl.uint = undefined;
@@ -49,8 +45,11 @@ var VBO: gl.uint = undefined;
 var EBO: gl.uint = undefined;
 
 var shader: Shader = undefined;
+var minus: u64 = 0;
 
 pub fn init() void {
+    minus = rgfw.RGFW_getTimeNS();
+
     {
         gl.GenBuffers(1, @ptrCast(&VBO));
 
@@ -84,57 +83,32 @@ pub fn init() void {
             defer gl.BindBuffer(gl.ARRAY_BUFFER, 0);
 
             gl.EnableVertexAttribArray(0);
-            gl.VertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), 0);
+            gl.VertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 6 * @sizeOf(f32), 0);
+            gl.EnableVertexAttribArray(1);
+            gl.VertexAttribPointer(1, 3, gl.FLOAT, gl.FALSE, 6 * @sizeOf(f32), 3 * @sizeOf(f32));
         }
     }
 
     shader = Shader.create(vertex_shader_source, fragment_shader_source);
 }
 
-var draw_square: bool = true;
-
 pub fn render() void {
     gl.ClearColor(0.2, 0.3, 0.3, 1.0);
     gl.Clear(gl.COLOR_BUFFER_BIT);
 
-    draw_square = !draw_square;
+    const elapsed: u64 = rgfw.RGFW_getTimeNS() - minus;
+    const second_float: f32 = @as(f32, @floatFromInt(elapsed)) / 1_000_000_000;
+    const green_value = @sin(second_float) / 2.0 + 0.5;
+
+    const location = gl.GetUniformLocation(shader.program, "ourColor");
 
     shader.use();
-
-    {
-        if (draw_square) {
-            {
-                gl.BindBuffer(gl.ARRAY_BUFFER, VBO);
-                defer gl.BindBuffer(gl.ARRAY_BUFFER, 0);
-
-                gl.BufferData(gl.ARRAY_BUFFER, @sizeOf(@TypeOf(vertices_square)), &vertices_square, gl.STATIC_DRAW);
-            }
-            {
-                gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO);
-                defer gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
-
-                gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, @sizeOf(@TypeOf(indices_square)), &indices_square, gl.STATIC_DRAW);
-            }
-        } else {
-            {
-                gl.BindBuffer(gl.ARRAY_BUFFER, VBO);
-                defer gl.BindBuffer(gl.ARRAY_BUFFER, 0);
-
-                gl.BufferData(gl.ARRAY_BUFFER, @sizeOf(@TypeOf(vertices_triangle)), &vertices_triangle, gl.STATIC_DRAW);
-            }
-            {
-                gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO);
-                defer gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, 0);
-
-                gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, @sizeOf(@TypeOf(indices_triangle)), &indices_triangle, gl.STATIC_DRAW);
-            }
-        }
-    }
+    gl.Uniform4f(location, 0.0, green_value, 0.0, 1.0);
 
     {
         gl.BindVertexArray(VAO);
         defer gl.BindVertexArray(0);
 
-        gl.DrawElements(gl.TRIANGLES, if (draw_square) 6 else 3, gl.UNSIGNED_INT, 0);
+        gl.DrawElements(gl.TRIANGLES, 3, gl.UNSIGNED_INT, 0);
     }
 }
