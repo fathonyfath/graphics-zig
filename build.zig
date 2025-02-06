765969c1b80451fbd136edc183e9a8472da52443
Fathony Teguh Irawan <4829378+fathonyfath@users.noreply.github.com>
2025-02-06 20:09:24 +0900

    Update to zig 0.14.0 with build code update

--- build.zig
+++ build.zig
@@ -4,16 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "graphics-zig",
+    const module = b.addModule("module", .{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    addRGFW(b, exe, target);
-    addOpenGL(b, exe);
-    addStbImage(b, exe);
-    addZm(b, exe);
+
+    addRGFW(b, module, target);
+    addOpenGL(b, module);
+    addStbImage(b, module);
+    addZm(b, module);
+
+    const exe = b.addExecutable(.{
+        .name = "graphics-zig",
+        .root_module = module,
+    });
 
     b.installArtifact(exe);
 
@@ -28,14 +33,8 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = module,
     });
-    addRGFW(b, exe_unit_tests, target);
-    addOpenGL(b, exe_unit_tests);
-    addStbImage(b, exe_unit_tests);
-    addZm(b, exe_unit_tests);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
@@ -43,50 +42,50 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_exe_unit_tests.step);
 }
 
-fn addRGFW(b: *std.Build, compile: *std.Build.Step.Compile, target: std.Build.ResolvedTarget) void {
-    compile.linkLibC();
-    compile.addIncludePath(b.path("vendor/rgfw"));
-    compile.addCSourceFile(.{
+fn addRGFW(b: *std.Build, module: *std.Build.Module, target: std.Build.ResolvedTarget) void {
+    module.link_libc = true;
+    module.addIncludePath(b.path("vendor/rgfw"));
+    module.addCSourceFile(.{
         .file = b.path("src/rgfw.c"),
     });
 
     if (target.result.os.tag == .windows) {
-        compile.linkSystemLibrary("gdi32");
-        compile.linkSystemLibrary("shell32");
-        compile.linkSystemLibrary("opengl32");
-        compile.linkSystemLibrary("winmm");
-        compile.linkSystemLibrary("user32");
+        module.linkSystemLibrary("gdi32", .{});
+        module.linkSystemLibrary("shell32", .{});
+        module.linkSystemLibrary("opengl32", .{});
+        module.linkSystemLibrary("winmm", .{});
+        module.linkSystemLibrary("user32", .{});
     } else if (target.result.os.tag == .linux) {
-        compile.linkSystemLibrary("X11");
-        compile.linkSystemLibrary("Xcursor");
-        compile.linkSystemLibrary("GL");
+        module.linkSystemLibrary("X11", .{});
+        module.linkSystemLibrary("Xcursor", .{});
+        module.linkSystemLibrary("GL", .{});
     } else if (target.result.os.tag == .macos) {
-        compile.linkFramework("Cocoa");
-        compile.linkFramework("OpenGL");
-        compile.linkFramework("IOKit");
+        module.linkFramework("Cocoa", .{});
+        module.linkFramework("OpenGL", .{});
+        module.linkFramework("IOKit", .{});
     } else {
         @panic("Unsupported target OS");
     }
 }
 
-fn addOpenGL(b: *std.Build, compile: *std.Build.Step.Compile) void {
+fn addOpenGL(b: *std.Build, module: *std.Build.Module) void {
     const gl_bindings = @import("zigglgen").generateBindingsModule(b, .{
         .api = .gl,
         .version = .@"4.1",
     });
 
-    compile.root_module.addImport("gl", gl_bindings);
+    module.addImport("gl", gl_bindings);
 }
 
-fn addStbImage(b: *std.Build, compile: *std.Build.Step.Compile) void {
-    compile.linkLibC();
-    compile.addIncludePath(b.path("vendor/stb"));
-    compile.addCSourceFile(.{
+fn addStbImage(b: *std.Build, module: *std.Build.Module) void {
+    module.link_libc = true;
+    module.addIncludePath(b.path("vendor/stb"));
+    module.addCSourceFile(.{
         .file = b.path("src/stb_image.c"),
     });
 }
 
-fn addZm(b: *std.Build, compile: *std.Build.Step.Compile) void {
+fn addZm(b: *std.Build, module: *std.Build.Module) void {
     const zm = b.dependency("zm", .{});
-    compile.root_module.addImport("zm", zm.module("zm"));
+    module.addImport("zm", zm.module("zm"));
 }
